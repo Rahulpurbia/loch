ae7c1afacc5851c58b6ae6de0b581c0ddcd4d15e
RahulPurbia11 <108067767+RahulPurbia11@users.noreply.github.com>
2023-12-07 13:49:41 +0530

    Add dummy data and map the values

--- src/components/TestimonialCard/TestimonialCard.js
+++ src/components/TestimonialCard/TestimonialCard.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./TestimonialCard.css";
-const TestimonialCard = () => {
+const TestimonialCard = ({name,title,testimonial}) => {
   return (
     <div className="testimonial-card">
       <div className="d-flex gap-1 align-items-center">
-        <div className="font-500">Rahul P.</div>
-        <div className="position-text">Ex Prime Minister</div>
+        <div className="font-500">{name}</div>
+        <div className="position-text">{title}</div>
       </div>
       <div className="testimonial-text mt-10">
-        “Love how Loch integrates portfolio analytics and whale watching into
-        one unified app.”
+       {testimonial}
       </div>
     </div>
   );
--- src/components/TestimonialCarousel/TestimonialCarousel.js
+++ src/components/TestimonialCarousel/TestimonialCarousel.js
@@ -2,14 +2,28 @@ import React from 'react'
 import './TestimonialCarousel.css'
 import TestimonialCard from "../TestimonialCard/TestimonialCard"
 
+const testimonial=[
+    {
+        name:"Rahul.P.",
+        title:"Frontend Developer",
+        testimonial:"“Love how Loch integrates portfolio analytics and whale watching into one unified app.”"
+    },
+    {
+        name:"Sanjay.P.",
+        title:"Fullstack Developer",
+        testimonial:"“I use Loch everyday now. I don't think I could analyze crypto whale trends markets without it. I'm addicted!”"
+    },
+    {
+        name:"Someone.P.",
+        title:"Backend Developer",
+        testimonial:"“Managing my own portfolio is helpful and well designed. What’s really interesting is watching the whales though. No one else has made whale tracking so simple.”"
+    },
+]
+
 const TestimonialCarousel = () => {
   return (
     <div className="testimonial-carousel hide-scrollbar mt-30">
-        <TestimonialCard/>
-        <TestimonialCard/>
-        <TestimonialCard/>
-        <TestimonialCard/>
-        <TestimonialCard/>
+      {testimonial.map((testimonial,index)=>   <TestimonialCard key={`${index}-testimonial`} name={testimonial.name} title={testimonial.title} testimonial={testimonial.testimonial}/>)}
     </div>
   )
 }
